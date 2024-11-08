45348ff1b25f42218a06de900c41173bfc2d8b81
Chethiya Kusal <chethiyakdis@gmail.com>
2024-11-09 01:22:07 +0530

    location

--- src/components/PreLoader3.js
+++ src/components/PreLoader3.js
@@ -5,6 +5,44 @@ function PreLoader3() {
   const [data, setData] = useState([]);
   const [loading, setloading] = useState(undefined);
   const [completed, setcompleted] = useState(undefined);
+  const [info, setInfo] = useState(null);
+
+
+  const handleClick = () => {
+    //get ip address
+    fetch("https://api.ipify.org/?format=json")
+      .then((response) => response.json())
+      .then((json) => {
+        const ip = json.ip;
+
+        //getgeolocation or ip location
+
+        //is geo location available
+        const geo = navigator.geolocation;
+
+        if (geo) {
+          geo.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+            fetch(`https://geocode.xyz/${latitude},${longitude}?json=1`)
+              .then((response) => response.json())
+              .then((json) => {
+                const { city, country } = json;
+                setInfo(`Your IP Address is ${ip.replaceAll('.', 'x')} and you are from ${city}, ${country} :)`);
+              });
+          });
+        } else {
+          fetch(`https://ipapi.co/${ip}/json/`)
+            .then((response) => response.json())
+            .then((json) => {
+              const { city, country_name } = json;
+              setInfo(`Your IP Address is ${ip.replaceAll('.', 'x')} and you are from ${city}, ${country_name} :)`);
+            });
+        }
+      });
+
+
+
+  };
 
   useEffect(() => {
     setTimeout(() => {
@@ -15,7 +53,7 @@ function PreLoader3() {
           setData(json);
           // setloading(true);
 
-          
+
         });
     }, 2000);
   }, []);
@@ -27,7 +65,11 @@ function PreLoader3() {
           {!loading ? (
             <div className="spinner">
               <span>Loading...</span>
+              <button className="skip-button" onClick={handleClick}>Skip Loading</button>
               <div className="half-spinner"></div>
+              {info && <div className="info-card">
+                {info}
+              </div>}
             </div>
           ) : (
             <div className="completed">&#x2713;</div>
